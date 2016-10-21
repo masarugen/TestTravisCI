07a4e020887f6ebcdb830599af6d3d6372b6391e
masaru-hashizume <masaru.hashizume@speee.jp>
2016-10-21 12:18:53 +0900

    fix

--- .travis_scripts/auto_generate.sh
+++ .travis_scripts/auto_generate.sh
@@ -15,11 +15,15 @@ if [[ $TRAVIS_EVENT_TYPE = "api" ]] ; then
     $GET_FILE_BRANCH \
     doc/schfile/test/time.txt \
     ./docs/time.txt
+    # 正常終了チェック
+    if [ $? -ne 0]; then
+        exit 1
+    fi
 
     # ソース生成
     mkdir -p ./docs/generate/
     cp ./docs/time.txt ./docs/generate/time.txt
-    `date` > ./docs/generate/date.txt
+    `date "+%Y-%m-%d_%H-%M-%S"` > ./docs/generate/date.txt
 
     # PRの生成
     ./.travis_scripts/create_pr.sh \
@@ -27,4 +31,8 @@ if [[ $TRAVIS_EVENT_TYPE = "api" ]] ; then
     $APP_DEVELOP_BRANCH \
     ./docs/generate \
     ./src/docs/generate
+    # 正常終了チェック
+    if [ $? -ne 0]; then
+        exit 1
+    fi
 fi
