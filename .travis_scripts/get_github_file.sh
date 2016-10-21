ecf5444a7ab3b60fd2265349d3f3db9ee25e4392
masaru-hashizume <masaru.hashizume@speee.jp>
2016-10-21 11:56:04 +0900

    fix

--- .travis_scripts/get_github_file.sh
+++ .travis_scripts/get_github_file.sh
@@ -15,7 +15,7 @@ GH_PATH="${4}"
 GH_URL="https://api.github.com/repos/${GH_OWNER}/${GH_REPO}/contents/${GH_PATH}?ref=${GH_BRANCH}"
 LOCAL_SAVE_PATH=${5}
 
-echo "${GH_OWNER} ${GH_REPO} ${}GH_BRANCH ${GH_PATH} ${LOCAL_SAVE_PATH}"
+echo "${GH_OWNER} ${GH_REPO} ${GH_BRANCH} ${GH_PATH} ${LOCAL_SAVE_PATH}"
 curl -s -f \
     -H "Authorization: token ${GH_TOKEN}" \
     -H "Accept: application/vnd.github.v3.raw" \
